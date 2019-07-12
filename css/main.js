3160159dfde16547f36fa575f6f225962ad2e895
cdrosales <cara10denise@hotmail.com>
2019-07-12 09:33:38 -0400

    fix: updated font style for headings

---
+++ css/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () { // waits for DOMContent(everything on page) to load first
+
+// const listedItems = document.querySelector('li');
+// console.log(listedItems[2])
+
+
+
+});

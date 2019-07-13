76240628caec16efda9b979227d867fd1cbf9375
cdrosales <cara10denise@hotmail.com>
2019-07-13 00:52:19 -0400

    chore: added JS and alert for email subscription

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+});
+
+const emailForm = document.getElementById("form")
+
+emailForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById("emailInfo")
+
+    if (emailInfo, true) {
+        alert("Thanks for Subscribing!")
+    }
+
+
+})

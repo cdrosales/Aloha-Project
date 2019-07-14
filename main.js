84acc7f99262a17029b89a91a19038bcb1506ec9
cdrosales <cara10denise@hotmail.com>
2019-07-14 19:40:08 -0400

    fix: updated div class names for featured products

--- main.js
+++ main.js
@@ -2,26 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-const emailForm = document.getElementById("form")
-
-emailForm.addEventListener("submit", function(event) {
-    event.preventDefault(); 
-
-    const email = document.getElementById("emailInfo")
-
-    if (emailInfo, true) {
-        alert("Thanks for Subscribing!")
-    }
-
-
-})
-
-
 //FLICKITY 
 
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.featuredProducts');
 var flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
   contain: true,
   groupCells: true,
@@ -29,9 +13,19 @@ var flkty = new Flickity( elem, {
 });
 
 
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+
+const emailForm = document.getElementById("form")
+
+emailForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const emailInfo = document.getElementById("emailInfo")
+
+    if (emailInfo = true) {
+        alert("Thanks for Subscribing!")
+    }
+
+})
 
 
 

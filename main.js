1203fe3d88f20991c34e88e072ffb272789353f9
cdrosales <cara10denise@hotmail.com>
2019-07-14 16:35:17 -0400

    chore: added feature products with flickity plugin

--- main.js
+++ main.js
@@ -23,12 +23,16 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  groupCells: true,
+  pageDots: true,
 });
 
-// element argument can be a selector string
-//   for an individual element
+
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
 
+
+
+// SMOOTH SCROLLING
